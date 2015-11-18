86ce791da7cdaa9a8054706aeb233f42791e4631
DaynBrandonCarretoBonilla <damcb164e9@gmail.com>
2015-11-18 12:56:41 -0600

    13

--- js/acciones.js
+++ js/acciones.js
@@ -4,18 +4,10 @@ function consulta()
 
 		$.ajax({
 			type: "POST",
-			url: "http://192.168.1.30/procesar.php",
-			success: function(msj){
-				alert ("exito" + msj);
-			},
-			error: function(xhr, ajaxOptions, throwError)
-			{
-				alert ("estatus" +xhr.sttus);
-			 alert ("error" +throwError);	
-			},
-			complete: function(data){
-			 	alert (json.ecode (data));
-			}
+			url: "192.168.1.30/procesar.php"
+		}).done(function(msj){
+			alert(mensaje);
+		
 		});
 				
 
